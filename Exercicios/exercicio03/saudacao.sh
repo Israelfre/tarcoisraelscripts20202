4ae96054575c80ddfab035bc75fa6409c456c03a
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2020-12-15 21:41:27 -0300

    Correção.

--- Exercicios/exercicio03/saudacao.sh
+++ Exercicios/exercicio03/saudacao.sh
@@ -1,5 +1,5 @@
- #!/bin/bash
-
+#!/bin/bash
+# Correção: OK. Não é preciso utilizar o >> se você já usa o tee.
 echo "Olá $(whoami), Hoje é dia $(date +%d), do mês $(date +%B), do ano $(date +%G)." | tee -a >> saudacao.log
 
 
