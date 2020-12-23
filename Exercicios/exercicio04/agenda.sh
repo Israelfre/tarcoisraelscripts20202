7d5f4d2659b58c94fd4b7dbbdac071bd7940e08d
Israelfre <71731332+Israelfre@users.noreply.github.com>
2020-12-22 21:47:41 -0300

    Add files via upload

--- Exercicios/exercicio04/agenda.sh
+++ Exercicios/exercicio04/agenda.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 adc=${0}
-nome${1}
-email${2}
+nome=${1}
+email=${2}
 
 if [ ${adc} = 'adicionar' ]
 then
 	echo "${nome}:${email}" >> usuarios.db
 	echo "Contato ${nome} adicionado com sucesso"
 fi
-
 if [ ${adc} = 'listar' ]
 then
 	cat usuarios.db
