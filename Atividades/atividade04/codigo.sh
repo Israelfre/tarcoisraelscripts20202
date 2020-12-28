2dc2213fab1f9eb008d8e283a92635694d485514
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2020-12-28 10:57:09 -0300

    Correção.

--- Atividades/atividade04/codigo.sh
+++ Atividades/atividade04/codigo.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Correção: 0,6
 
 sed -i 's/\/bin\/python/\/usr\/\bin\/\python3/g' atividade04.py
 
+# notaFinal não vira NOTAFINAL
 sed -i 's/nota/\U&/g' atividade04.py
 
 sed -i '4s/$/import time/' atividade04.py
 
+# Não é um comando sed
 echo "print(time.ctime())" >> atividade04.py 
 
 
