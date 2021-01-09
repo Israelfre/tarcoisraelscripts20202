f7b287847c98ff5cd3a2df0b0c9f55764e34fde8
Israelfre <71731332+Israelfre@users.noreply.github.com>
2021-01-09 12:34:23 -0300

    Add files via upload

--- Atividades/atividade05/infoDir.sh
+++ Atividades/atividade05/infoDir.sh
@@ -6,5 +6,5 @@ if test -d $dir
 then
         echo "O diretorio $dir ocupa $(du -hsk $dir | sed 's/[[:space:]].*.*/ /') kilobytes e tem $(ls $dir | wc -l) itens."
 else
-	echo "Não é um diretorio"
+	echo "$dir Não é um diretorio"
 fi
