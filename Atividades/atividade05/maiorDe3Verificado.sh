6a4f6af1779e91f7cb69fcee265198ddeb13ab2d
Israelfre <71731332+Israelfre@users.noreply.github.com>
2021-01-09 11:16:41 -0300

    Add files via upload

---
+++ Atividades/atividade05/maiorDe3Verificado.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+num1=$1
+num2=$2
+num3=$3
+
+if [[ $num1 = ?+([A-Za-z]) ]]
+then
+        echo "Opa!! $num1 não é número."
+
+        elif [[ $num2 = ?+([A-Za-z]) ]]
+        then
+                echo "Opa!! $num2 não é número."
+
+        elif [[ $num3 = ?+([A-Za-z]) ]]
+        then
+                echo "Opa!! $num3 não é número."
+
+
+	elif test $num1 -ge $num2 && test $num1 -ge $num3
+	then
+        	echo "$num1"
+
+		elif test $num2 -ge $num1 && test $num2 -ge $num3
+		then
+        		echo "$num2"
+
+		elif test $num3 -ge $num1 && test $num3 -ge $num2
+		then
+	      		echo "$num3"
+		fi
+
