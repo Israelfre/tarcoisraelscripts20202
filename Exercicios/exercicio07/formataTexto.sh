be9f15100cca339ff45c05179ca59b9586cc445c
Israelfre <71731332+Israelfre@users.noreply.github.com>
2021-01-26 20:27:10 -0300

    Add files via upload

---
+++ Exercicios/exercicio07/formataTexto.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+FORMATO=$1
+COR=$2
+SEP=$3
+TXT=$4
+
+case $FORMATO in
+  "sublinhado")
+	  tput smul
+	  ;;
+  "negrito")
+	  tput bold
+	  ;;
+  "reverso")
+	  tput smso
+          ;;	  
+esac
+
+tput setaf $COR
+
+linha=`echo $SEP | cut -f1 -d','`
+coluna=`echo $SEP | cut -f2 -d','`
+
+tput cup $linha $coluna
+
+echo $TXT
+tput init
+
