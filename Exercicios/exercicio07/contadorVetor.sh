2f7459b716f52c05b733174a8c4989f59412af66
Israelfre <71731332+Israelfre@users.noreply.github.com>
2021-01-26 21:45:06 -0300

    Add files via upload

---
+++ Exercicios/exercicio07/contadorVetor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+Num=()
+ind=0
+
+while [ true ]
+do 
+	read -p "Digite o numero no vetor, ou liste o tamanho: " recebe
+	if test $recebe != "q"
+	then
+		num[$ind]=$recebe
+		ind=$((ind+1))
+			
+		echo "Num adc com sucesso"
+	elif test $recebe == "q"
+	then
+		echo "Tamanho do vetor é ${#num[@]}"
+		break
+	else
+		echo "Comando não é reconhecido"
+	fi
+done
