65f263c85ef5b40ace0d4d55aa063a4003e6fe07
Israelfre <71731332+Israelfre@users.noreply.github.com>
2021-02-03 10:30:31 -0300

    Add files via upload

---
+++ Atividades/atividade10/contaPalavras.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ 
+read -p "Informe o arquivo: " arquivo
+
+declare -A vet
+
+for i in `cat $arquivo | tr '[[:punct:]]' ' ' | tr '[[:upper:]]' '[[:lower:]]'`
+do
+	  let vet[$i]++
+
+done
+
+for i in `cat $arquivo | tr '[[:punct:]]' ' ' | tr ' ' '\n' | tr '[[:upper:]]' '[[:lower:]]' | sort | uniq`
+do
+	  echo "$i: ${vet[$i]}" >> palavras.txt
+
+done
+
+cat palavras.txt | awk '{ print $2 " " $1}' | sort -r | awk '{ print $2 " " $1}'
+
+rm palavras.txt
+
